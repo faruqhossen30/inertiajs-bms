ebe5b8e594975d70269a2a774fe061760237a2c5
faruqhossen30 <faruqhossen30@gmail.com>
2023-09-29 11:20:53 +0600

    first commit

---
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -0,0 +1,20 @@
+import Navbar from '@/Components/Dashboard/Navbar';
+
+import Sidebar from '@/Components/Dashboard/Sidebar';
+import { useState } from 'react';
+
+export default function Authenticated({ user, header, children }) {
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+
+    return (
+        <div className="bg-gray-50 dark:bg-slate-900">
+            <Sidebar />
+            <div className="w-full lg:pl-64">
+                <Navbar />
+                <div className="bg-white dark:bg-gray-800 dark:text-slate-400 p-2 m-2">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
